fdadd87adb1fd7334ab696665958944ad16817d4
Pip <pip.harsveld@hva.nl>
2023-04-17 20:10:54 +0200

    Add users and typing state
    - Remove cookies
    - Add names
    - Add typing state

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import http from 'http';
-import {Server} from 'socket.io';
-import cookieParser from 'cookie-parser';
+import { Server } from 'socket.io';
+// import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -12,41 +12,28 @@ const port = process.env.PORT || 4200;
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.resolve('public')));
 
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    socket.on('message', (message) => {
-        // while (history.length > historySize) {
-        //   history.shift()
-        // }
-        // history.push(message)
-    
-        io.emit('message', message)
-      })
-    
-      socket.on('disconnect', () => {
+    socket.on('chat', (data) => {
+        console.log(data);
+        io.sockets.emit("chat", data);
+    });
+
+    socket.on('typing', (inputName) => {
+        console.log("Aan het typen");
+        socket.broadcast.emit("typing", inputName);
+    });
+
+    socket.on('disconnect', () => {
         console.log('user disconnected')
-      })
+    })
 });
 
-// io.on('connection', socket => {
-//     console.log(socket.request) 
-//     // const username = socket.request.cookies.username;
-//     // socket.emit('joined', { username });
-//     console.log('a user connected');
-  
-//     socket.on('chat message', msg => {
-//       io.emit('chat message', { username, msg });
-//     });
-
-//     socket.on('disconnect', () => {
-//         console.log('user disconnected')
-//     })
-// });
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
