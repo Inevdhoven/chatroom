ba8f186c6dc1935f8b6c76146a231fc3ab233f28
Eva Zaadnoordijk <evazaadnoordijk@gmail.com>
2023-04-17 13:11:17 +0200

    comments

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ let socket = io();
 let messages = document.querySelector('section ul')
 let input = document.querySelector('input')
 
+// send text
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault()
   if (input.value) {
@@ -10,6 +11,7 @@ document.querySelector('form').addEventListener('submit', event => {
   }
 })
 
+// received text
 socket.on('message', message => {
     // console.log("message")
   messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }))
